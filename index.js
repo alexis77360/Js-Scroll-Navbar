5f32a0bccee49d06bc319661eb2566ca0b8aeda9
alexis77360 <84974466+alexis77360@users.noreply.github.com>
2023-06-28 16:48:40 +0200

    final

---
+++ index.js
@@ -0,0 +1,13 @@
+let lastScroll = 0;
+
+window.addEventListener("scroll", () => {
+
+    if(window.scrollY < lastScroll) {
+        navbar.style.top = "0";
+    } else {
+        navbar.style.top = "-100px";
+    }
+    
+   lastScroll = window.scrollY;
+
+});
